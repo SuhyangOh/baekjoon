6df60aa15c85be4ebd5d08e75ea3d41794fc7f95
오수향 <suhyang@osuhyangs-MacBook-Pro.local>
2021-09-03 12:14:01 +0900

    A+B - 4

---
+++ 210902/Baekjoon10951.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class  Baekjoon10951 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println(a + b);
+        }
+    }
+}
